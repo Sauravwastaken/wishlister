cc610c01c59acde86e01f31df2952b7b339a0d3d
Sauravwastaken <sauravthecoder@gmail.com>
2025-04-18 15:17:16 +0530

    Added server configurations

--- parts/_dbconnect.php
+++ parts/_dbconnect.php
@@ -1,14 +1,25 @@
 <?php
 
-$server = "localhost";
-$host = "root";
-$password = "";
-$database = "wishlister";
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    $base_url = "http://localhost/wishlister/";
+    $server = 'localhost';
+    $user = 'root';
+    $password = "";
+    $database = 'wishlister';
+} else {
+    $base_url = "http://206.189.129.236/";
+    $server = "localhost";
+    $user = "root";
+    $password = "wP59IT`I9N1F";
+    $database = "wishlister";
+}
 
-$connect = mysqli_connect($server, $host, $password, $database);
+$connect = mysqli_connect($server, $user, $password, $database);
 
-if(!$connect) {
-    echo "Could not connect".mysqli_connect_error();
+
+
+if (!$connect) {
+    echo "Could not connect" . mysqli_connect_error();
 }
 
 session_start();
